c27ccbb582d363daf793e13c58e06bc991338262
Joakim Carlstein <joakim@klei.se>
2015-07-21 12:53:49 +0200

    reduce: wait before continue
    closes GH-1
    
    Adding test to verify #1

--- test.js
+++ test.js
@@ -2,17 +2,32 @@ const test    = require('tape')
 const reduce     = require('./')
 const Promise = require('any-promise')
 
-test('promise-reduce should assert input types', function(t) {
+test('promise-reduce should assert input types', function (t) {
   t.plan(1)
   t.throws(reduce.bind(null, 123))
 })
 
-test('promise-reduce should reduce a fn', function(t) {
+test('promise-reuduce should accept a single val', function (t) {
+  t.plan(1)
+
+  const res = Promise.resolve(2)
+    .then(reduce(reduceFn, 3))
+    .then(checkFn)
+
+  function reduceFn(prev, next) {
+    return prev + next
+  }
+
+  function checkFn(val) {
+    t.equal(5, val)
+  }
+})
+
+test('promise-reduce should reduce a fn', function (t) {
   t.plan(1)
   const res = Promise.resolve([1, 2, 3])
     .then(reduce(reduceFn, 0))
     .then(checkFn)
-    .catch(handleErr)
 
   function reduceFn(prev, next) {
     return prev + next
@@ -21,8 +36,23 @@ test('promise-reduce should reduce a fn', function(t) {
   function checkFn(val) {
     t.equal(6, val)
   }
+})
 
-  function handleErr() {
-    t.fail('catch')
+test('should not continue until last iteration has been resolved', function (t) {
+  t.plan(1)
+  const res = Promise.resolve([1, 2, 3])
+    .then(reduce(reduceFn, 0))
+    .then(checkFn)
+
+  function reduceFn(prev, next) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        resolve(prev + next)
+      }, 1)
+    })
+  }
+
+  function checkFn(val) {
+    t.equal(6, val)
   }
 })
